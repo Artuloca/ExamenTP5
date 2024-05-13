8abd9d4df549682da950bccb1e28026f24f89c74
Arturo <artuloca@myuax.co>
2024-05-13 14:58:51 +0200

    patron singelton

---
+++ Restaurante/GestorDeRestaurantes.java
@@ -0,0 +1,16 @@
+package Restaurante;
+
+
+class GestorDeRestaurantes {
+    private static GestorDeRestaurantes instanciaUnica = new GestorDeRestaurantes();
+
+    private GestorDeRestaurantes() {}
+
+    public static GestorDeRestaurantes getInstancia() {
+        return instanciaUnica;
+    }
+
+    public void gestionar() {
+        System.out.println("Gestionando restaurantes...");
+    }
+}
